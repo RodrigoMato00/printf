dd2e79bd35e1530604febe380abe689f60059ef2
RodrigoMato00 <3293@holbertonschool.com>
2021-07-24 15:40:28 -0700

    commit5

--- holberton.h
+++ holberton.h
@@ -1,7 +1,19 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int print_char(va_list list, char *save);
+int print_str(va_list args, char *save);
+int print_percent(char a, char *save);
+
+int print_decimal(va_list args, char *save);
+int print_int(va_list args, char *save)
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 
 #endif
-
