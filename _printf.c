4ba7c25b041e9a6e4ba0706991219a53fb1eda50
fabianatellechea <fabianagtellechea@gmail.com>
2021-07-26 12:54:27 -0700

    correccion comentarios

--- _printf.c
+++ _printf.c
@@ -32,21 +32,25 @@ int _printf(const char *format, ...)
 		/*{"R", print_rot13},*/
 		{"s", print_str},
 		{NULL, NULL}
+		
+		/* este struct va a funcionar como selector de funciones, cada caracter es un placeholder */
 	};
-		/* este struct que se ve arriba va a funcionar como selector de funciones, cada caracter que se ve es un placeholder, y dependiendo del mismo, la funcion que va a realizar para printear */
 
 	bi = &i;
 	if (format == NULL)
-		return (-1); /* aca especificamos que si el format es null no haga nada(en vdd hace, sale con un -1 de status, que es error)*/
-	buffer = malloc(1024); /*creamos el espacio de memoria suficiente (más que suficiente con 1024 xD) para el string que vamos a printear*/
+		return (-1); /* si el format es null salga con un -1 de status, que es error)*/
+	buffer = malloc(1024); /*creamos el espacio de memoria mas que suficiente para el string que vamos a printear*/
 	if (buffer == NULL) /* idem que si el format es NULL*/
 		return (-1);
 	va_start(args, format); /* inicializamos la lista de argumentos variables que va a recibir la funcion printf*/
 	if (args == NULL)
-		return (-1); /* aca estamos diciendo que si no hay argumentos, esto incluye el format, de nuevo, NULL, status exit with error*/
-	counter = formats(format, spec, args, buffer, bi); /* aca usamos la funcion formats, que esta dentro de iterator.c, que basiscamente decide que funcion utilizar, a efectos de ver que printear, y esto se hace con el struct, y recorriendo chars que tiene para determinar la funcion*/
-	write(1, buffer, *bi); /* llamada de muy low level a una funcion del sistema( una syscall) que se llama write, en donde se especifica lo que se escribe, y lo que pesa en bytes ( que se pasa con el puntero de bi)*/
+		return (-1); /* si no hay argumentos, esto incluye el format, de nuevo, NULL, status exit with error*/
+	counter = formats(format, spec, args, buffer, bi); 
+	/* aca usamos la funcion formats, que esta dentro de iterator.c, que basicamente decide que funcion utilizar,*/
+	/*a efectos de ver que printear, y esto se hace con el struct, y recorriendo chars que tiene para determinar la funcion*/
+	write(1, buffer, *bi); /*llamada de muy low level a una funcion del sistema( una syscall) que se llama write,*/
+				/*en donde se especifica lo que se escribe, y lo que pesa en bytes ( que se pasa con el puntero de bi)*/
 	free(buffer); /*liberamos el espacio de memoria generado para buffer*/
 	va_end(args); /*aca finalizamos el uso de las listas variadicas*/
-	return (counter); 
+	return (counter);
 }
